1ddf0254bff9bfcf6b22c7e2b209c63fccdb39ad
Nick <bridgetmcknight@Bridgets-MacBook-Pro.local>
2021-12-11 18:42:19 -0800

    Basic Layout

---
+++ 02-java-oop/09-phone/PhoneTester.java
@@ -0,0 +1,17 @@
+public class PhoneTester {
+    public static void main(String[] args) {
+        Galaxy s9 = new Galaxy("S9", 99, "Verizon", "Ring Ring Ring!" );
+        Iphone iphoneTen = new Iphone("x", 100, "AT&T", "The Best");
+
+        s9.displayInfo();
+        System.out.println(s9.ring());
+        System.out.println(s9.unlock());
+
+        iphoneTen.displayInfo();
+        System.out.println(iphoneTen.ring());
+        System.out.println(iphoneTen.unlock());
+        
+
+    }
+        
+}
